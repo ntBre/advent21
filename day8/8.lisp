4f80af59b18849ea664a85c7059271fd4301dd29
Brent Westbrook <brentrwestbrook@gmail.com>
2021-12-09 10:49:22 -0600

    finish day 8

--- day8/8.lisp
+++ day8/8.lisp
@@ -10,6 +10,9 @@
     (a b c d e f g)
     (a b c d f g)))
 
+(defun parts->num (parts)
+    (position parts *nums* :test #'equal))
+
 ;; length to number
 ;; 2 => 1
 ;; 3 => 7
@@ -39,14 +42,18 @@ lengths"
 
 ;; have to map from one set of characters to another
 
+(defun str->syms (str)
+  "convert a string of characters to a sorted list of the
+corresponding symbols"
+  (mapcar #'read-from-string
+	  (sort (loop for c across str
+		      collecting (string c))
+		#'string<)))
+
 (defun convert (str)
   "convert a string of characters to a binary number"
   (let ((ret 0)
-	(letters
-	  (mapcar #'read-from-string
-		  (sort (loop for c across str
-			      collecting (string c))
-			#'string<))))
+	(letters (str->syms str)))
     (loop for i from 7 downto 0
 	  do (setf ret
 		   (logior ret
@@ -56,18 +63,15 @@ lengths"
     ret))
 
 (defun parse2 (filename)
-  (mapcar #'butlast
-	  (mapcar #'uiop:split-string
-		  (with-open-file (in filename)
-		    (loop for line = (read-line in nil nil)
-			  while line
-			  collecting (car (uiop:split-string line :separator "|")))))))
-
-(defun part2 ()
-  (let ((*print-base* 2))
-    (loop for l in
-		(mapcar #'convert (car (parse2 "one.txt")))
-	  do (print-bin l))))
+  (with-open-file (in filename)
+    (loop for line = (read-line in nil nil)
+	  while line
+	  collecting
+	  (mapcar #'(lambda (x)
+		      (remove-if #'(lambda (str)
+				     (string= "" str))
+				 (uiop:split-string x)))
+		  (uiop:split-string line :separator "|")))))
 
 (defun fiver (str)
   (= 5 (length str)))
@@ -77,32 +81,6 @@ lengths"
 
 (defun print-bin (num)
   (format t  "~7,'0b~%" num))
-
-(let ((zero  #b1110111)
-      (one   #b0010010)
-      (two   #b1011101)
-      (three #b1011011)
-      (four  #b0111010)
-      (five  #b1101011)
-      (six   #b1101111)
-      (seven #b1010010)
-      (eight #b1111111)
-      (nine  #b1111011))
-  (format t "abcdefg~%")
-  (print-bin (logxor one seven)) ;; -> a
-  (print-bin (logand zero six nine one)) ;; f
-  (print-bin (logxor one (logand zero six nine one))) ;; c
-  (print-bin (logand two three five four)) ;; d
-  (print-bin (logxor four one (logand two three five four))) ;; b
-  (print-bin (logand zero six nine))
-  )
-  ;; xor 1-7 gives a
-  ;; (print-bin (logand two three five))
-  ;; (print-bin (logxor two five))
-  ;; (print-bin (logxor two three five))
-  ;; (print-bin (logxor zero six nine))
-  ;; (print-bin (logxor two three five
-  ;; 		     zero six nine)))
 
 (defun get-fivers (elt)
   (remove-if-not #'fiver elt))
@@ -125,30 +103,48 @@ lengths"
 	    (#b0000001 . g))))
   (cdr (assoc mask keys))))
 
-(let* ((input (car (parse2 "one.txt")))
-       (one (find-n 2 input))
-       (four (find-n 4 input))
-       (seven (find-n 3 input))
-       (eight (find-n 7 input))
-       (fivers (mapcar #'convert (get-fivers input)))
-       (sixers (mapcar #'convert (get-sixers input)))
-       (a (logxor one seven)) ; actually d
-       (f (apply #'logand one sixers)) ; actually e
-       (c (logxor f one)) ; actually a
-       (d (apply #'logand four fivers)) ; f
-       (b (logxor d four one)) ; g
-       (g (apply #'logxor a b f sixers))
-       (e (logxor #b1111111 a b c d f g)))
-  (print-bin (logior d g)) ; actually 1
-  (print-bin a)
-  (print-bin b)
-  (print-bin c)
-  (print-bin d)
-  (print-bin e)
-  (print-bin f)
-  (print-bin g))
+(defun make-translator (input)
+  (let* ((one (find-n 2 input))
+	 (four (find-n 4 input))
+	 (seven (find-n 3 input))
+	 (eight (find-n 7 input))
+	 (fivers (mapcar #'convert (get-fivers input)))
+	 (sixers (mapcar #'convert (get-sixers input)))
+	 (a (logxor one seven))
+	 (f (apply #'logand one sixers))
+	 (c (logxor f one))
+	 (d (apply #'logand four fivers))
+	 (b (logxor d four one))
+	 (g (apply #'logxor a b f sixers))
+	 (e (logxor eight a b c d f g)))
+    (lambda (x)
+      (let ((keys
+	      `((,(extract-key a) . a)
+		(,(extract-key b) . b)
+		(,(extract-key c) . c)
+		(,(extract-key d) . d)
+		(,(extract-key e) . e)
+		(,(extract-key f) . f)
+		(,(extract-key g) . g))))
+	(cdr (assoc x keys))))))
 
-;; compute masks from these and see what I can extract from that
+(defun work (training code)
+  (labels ((translator (x)
+	     (funcall (make-translator training) x)))
+    (mapcar #'parts->num
+	    (mapcar #'(lambda (x)
+			(sort (mapcar
+			       #'translator (str->syms x))
+			      #'(lambda (x y)
+				  (string< (symbol-name x)
+					   (symbol-name y)))))
+		    code))))
 
-;; get letters corresponding to the input masks, then use those
-;; letters and the true masks to decode the message
+(defun list->num (lst)
+  (read-from-string
+   (apply #'uiop:strcat (mapcar #'write-to-string lst))))
+
+(defun part2 (filename)
+  (reduce #'+
+	  (loop for (training code) in (parse2 filename)
+		collecting (list->num (work training code)))))
